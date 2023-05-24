92bd35c0b6d1d28b8ee1cb5a046b4bb312366368
Adolfo20bm <abm2491@gmail.com>
2023-05-24 07:28:17 -0500

    compiolando sass congulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
-function tarea(done) {
-    console.log("mi primer tarea");
-    done();
+const {src, dest} = require("gulp");
+const sass = require('gulp-sass')(require('sass'));
+
+function css(done) {
+    src("src/scss/app.scss") // Identificamos el archivo SASS
+    .pipe(sass()) // Compilarlo
+    .pipe(dest("build/css")) // Almacenarla en el discoduro
+
+    done(); // Callback que avisa a gulp cuando llegamos al final
 }
 
-exports.primerTarea = tarea;
+exports.css = css;
